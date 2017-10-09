b79870fddf12692fcb5744b4ed31c19995e5fe60
tankorsmash <tankorsmash@gmail.com>
2017-10-09 17:05:51 -0400

    use grequests to bulk requests on getting item market data

--- freethrow.py
+++ freethrow.py
@@ -3,7 +3,7 @@
 import json
 import dateutil
 from operator import itemgetter
-import requests
+import grequests, requests
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,7 +25,7 @@ BASE_URL = "https://api.steamapis.com/"
 DEFAULT_CONTEXT = 2
 
 def _do_request(url):
-    response = requests.get(BASE_URL+url, params={"api_key": STEAMAPIS_APIKEY})
+    response = requests.get(url, params={"api_key": STEAMAPIS_APIKEY})
     try:
         response.raise_for_status()
     except Exception as e:
@@ -44,7 +44,7 @@ def match_asset_to_description(assets, descriptions, cid):
 
 def request_your_game_inventory(app_id):
     print('requesting your inventory for app id {}...'.format(app_id), end='')
-    url = "steam/inventory/{sid64}/{app_id}/{ctxt}/".format(
+    url = BASE_URL+"steam/inventory/{sid64}/{app_id}/{ctxt}/".format(
             sid64=STEAMID_64,
             app_id=PUBG_ID,
             ctxt=DEFAULT_CONTEXT)
@@ -81,13 +81,16 @@ def get_your_game_inventory(app_id, market_data=None):
 
     return items
 
-def request_game_market_data(app_id):
-    print('requesting static inventory for app id {}...'.format(app_id), end='')
-    url = "market/items/{app_id}/".format(
+def build_game_market_data_url(app_id):
+    return BASE_URL+"market/items/{app_id}/".format(
         app_id=PUBG_ID,
     )
-    print('done')
+
+def request_game_market_data(app_id):
+    print('requesting static inventory for app id {}...'.format(app_id), end='')
+    url = build_game_market_data_url(app_id)
     response = _do_request(url)
+    print('done')
     return response
 
 def get_game_market_data(app_id):
@@ -96,14 +99,17 @@ def get_game_market_data(app_id):
     market_data = models.GameMarketData(data)
     return market_data
 
-def request_market_data_for_app_and_name(app_id, market_hash_name):
-    print('market data for app id {} and item {}...'.format(app_id, market_hash_name), end='')
-    url = "market/item/{app_id}/{market_hash_name}".format(
+def build_market_data_url_for_item(app_id, market_hash_name):
+    return BASE_URL+"market/item/{app_id}/{market_hash_name}".format(
         app_id=PUBG_ID,
         market_hash_name=market_hash_name,
     )
-    print('done')
+
+def request_market_data_for_app_and_name(app_id, market_hash_name):
+    print('market data for app id {} and item {}...'.format(app_id, market_hash_name), end='')
+    url = build_market_data_url_for_item(app_id, market_hash_name)
     response = _do_request(url)
+    print('done')
     return response
 
 def get_market_data_for_app_and_name(app_id, market_hash_name):
@@ -111,7 +117,7 @@ def get_market_data_for_app_and_name(app_id, market_hash_name):
     data = response.json()
     return data
 
-def get_market_data_for_item(item):
+def get_and_fill_market_data_for_item(item):
     if not item.marketable:
         data = {}
     else:
@@ -119,30 +125,39 @@ def get_market_data_for_item(item):
     item.fill_market_data(data)
     return data
 
+def exception_handler(request, exception):
+    print exception
+    import ipdb; ipdb.set_trace(); #TODO
+
 def old_test():
+    print("starting old_test")
     pubg_market_data = get_game_market_data(PUBG_ID)
     items = get_your_game_inventory(PUBG_ID)
     bind_my_items_to_market_data(items, pubg_market_data)
     print ("found {} items".format(len(items)))
+
+    pooled_reqs = []
+    for item in items:
+        if item.marketable:
+            url = build_market_data_url_for_item(item.appid, item.market_hash_name)
+            pooled_reqs.append(grequests.get(url, params={"api_key": STEAMAPIS_APIKEY}))
+
+    print("start pooling {} requests...".format(len(pooled_reqs)), end=" ")
+    market_data = grequests.map(pooled_reqs, exception_handler=exception_handler)
+    market_data = [md.json() for md in market_data]
+    print("done")
+
     trendlines = []
     for item in items:
-        get_market_data_for_item(item)
-        prices = item.prices
-        histogram = item.market_data.histogram
-        median_data = item.market_data.median_avg_prices_15days
         if item.marketable:
+            data = list(filter(lambda d: d['market_hash_name'] == item.market_hash_name, market_data))
+            if data:
+                item.fill_market_data(data[0])
+
+            median_data = item.market_data.median_avg_prices_15days
             trendlines.append((item.market_hash_name, median_data.trendlines()))
 
     trendlines.sort(key=lambda tl: itemgetter(3)(tl[1]))
-    pp(trendlines[0])
-    pp(trendlines[-1])
-    # pp(median_data.data)
-    # x = list(map(dateutil.parser.parse, map(itemgetter(0), median_data)))
-    # y = list(map(itemgetter(2), median_data))
-    # plt.plot(x, y)
-    # plt.title("ASDASDA")
-    # plt.plot_date(median_data)
-    # response = request_game_inventory(PUBG_ID)
 
 def testing():
     pubg_market_data = get_game_market_data(PUBG_ID)
@@ -156,4 +171,4 @@ def testing():
 
 if __name__ == "__main__":
     old_test()
-    testing()
+    # testing()
